ec17b830a1dce928bb89dcaaf439e867fe6d0a6a
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-03-28 11:14:19 -0400

    Add Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 }
 
 dependencies {
@@ -43,11 +47,25 @@ dependencies {
     implementation ("androidx.core:core-ktx:1.7.0")
     implementation ("androidx.appcompat:appcompat:1.4.1")
     implementation ("com.google.android.material:material:1.5.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+    // implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+
+    // compose
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.material:material:1.1.1")
+    implementation("androidx.compose.animation:animation:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.1.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    implementation("androidx.navigation:navigation-compose:2.4.1")
+    implementation("androidx.compose.ui:ui:1.1.1")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha07")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.23.1")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.23.1")
 
     // navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+    // implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    // implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
 
     // toolbar
     implementation("androidx.appcompat:appcompat:1.4.1")
